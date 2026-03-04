f28a257f7754bfee693603ba6b53df0a527c0c44
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-03-04 16:49:43 +0000

    worked on portfolio task

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -22,8 +22,8 @@ int main(void){
 	for (j=0; j<10; j++) {
 		if (hex[j] != ("1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9" || "A" || "B" || "C" || "D" || "E" || "F")) {
 			printf("Error: Invalid Hexadecimal\n");
+			return 0;
 		}
-		return 0;
 	}
 	// printf("Error: Invalid Hexadecimal\n");
 	
@@ -33,52 +33,53 @@ int main(void){
 	for (i=0; i<10;i++);
 	    switch (i) {
 			case hex[i] == "1":
-			decimal += 1;
-			break;
+			    decimal += 1;
+			    break;
 			case hex[i] == "2":
-			decimal += 2;
-			break;
+			    decimal += 2;
+			    break;
 			case hex[i] == "3":
-			decimal += 3;
-			break;
+			    decimal += 3;
+			    break;
 			case hex[i] == "4":
-			decimal += 4;
-			break;
+			    decimal += 4;
+			    break;
 			case hex[i] == "5":
-			decimal += 5;
-			break;
+			    decimal += 5;
+			    break;
 			case hex[i] == "6":
-			decimal +=6;
-			break;
+			    decimal +=6;
+			    break;
 			case hex[i] == "7":
-			decimal +=7;
-			break;
+			    decimal +=7;
+			    break;
 			case hex[i] == "8":
-			decimal +=8;
-			break;
+			    decimal +=8;
+			    break;
 			case hex[i] == "9":
-			decimal +=9;
-			break;
-			case hex[i] == "A":
-			decimal += 10;
-			break;
-			case hex[i] == "B":
-			decimal += 11;
-			break;
-			case hex[i] == "C":
-			decimal += 12;
-			break;
-			case hex[i] == "D":
-			decimal += 13;
-			break;
-			case hex[i] == "E":
-			decimal += 14;
-			break;
-			case hex[i] == "F":
-			decimal += 15;
-			break;
-
-		}
+			    decimal +=9;
+			    break;
+			case hex[i] == ("A" || "a"):
+			    decimal += 10;
+			    break;
+			case hex[i] == ("B" || "b"):
+			    decimal += 11;
+			    break;
+			case hex[i] == ("C" || "c"):
+			    decimal += 12;
+			    break;
+			case hex[i] == ("D" || "d"):
+			    decimal += 13;
+			    break;
+			case hex[i] == ("E" || "e"):
+			    decimal += 14;
+			    break;
+			case hex[i] == ("F" || "f"):
+			    decimal += 15;
+			    break;
+			default:
+			    decimal += 0;
+	    }
 	printf("decimal:%ld\n", decimal);
 	
 	return 0;
