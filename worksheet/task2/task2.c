c8fcbe3fdc1704d528b05c1758e4231b97c08d61
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-03-08 19:20:51 +0000

    worked on portfolio task

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -13,73 +13,66 @@ int main(void){
 	long decimal=0;
 	char hex[9];
 	
-	printf("Enter a hexadecimal:");
-	scanf("%c", &*hex);
+	printf("Enter a hexadecimal:\n");
+    fgets(hex, sizeof(hex), stdin);
+	hex[strcspn(hex, "\n")] = '\0';
+	char *ptr = hex;
 
 
 	// if input contains invalid hex digit
-	int j = 0;
-	for (j=0; j<10; j++) {
-		if (hex[j] != ("1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9" || "A" || "B" || "C" || "D" || "E" || "F")) {
+	/*
+	for (int j=0; j<10; j++) {
+		if (hex[j] == (" " || "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9" || "A" || "B" || "C" || "D" || "E" || "F")) {
+			printf("Valid Hexadecimal\n");
+		} else {
+			printf(hex);
+		}
+	}
+	*/
+
+	// printf("Error: Invalid Hexadecimal\n");
+	// print the decimal result
+	
+	int len = strlen(hex);
+	while (*ptr != '\0') { 
+		if (ptr == "0") {
+			decimal = decimal;
+		} else if (ptr == "1") {
+			decimal += 1;
+		} else if (ptr == "2") {
+			decimal +=2;
+		} else if (ptr == "3") {
+			decimal +=3;
+		} else if (ptr == "4") {
+			decimal +=4;
+		} else if (ptr == "5") {
+			decimal +=5;
+		} else if (ptr == "6") {
+			decimal +=6;
+		} else if (ptr == "7") {
+			decimal += 7;
+		} else if (ptr == "8") {
+			decimal += 8;
+		} else if (ptr == "9") {
+			decimal += 9;
+		} else if (ptr == "A"||ptr == "a") {
+			decimal += 10;
+		} else if (ptr == "B") {
+			decimal +=11;
+		} else if (ptr == "C") {
+			decimal += 12;
+		} else if (ptr == "D") {
+			decimal += 13;
+		} else if (ptr == "E") {
+			decimal += 14;
+		} else if (ptr == "F") {
+			decimal += 15;
+		} else {
 			printf("Error: Invalid Hexadecimal\n");
 			return 0;
 		}
-		input_value = calloc(hex[j]);
+	    ptr++;
 	}
-	// printf("Error: Invalid Hexadecimal\n");
-	// print the decimal result
-	int sum = 0;
-    int i = 0;
-	for (i=0; i<10;i++);
-	    switch (i) {
-			case hex[i] == "1":
-			    decimal += 1;
-			    break;
-			case hex[i] == "2":
-			    decimal += 2;
-			    break;
-			case hex[i] == "3":
-			    decimal += 3;
-			    break;
-			case hex[i] == "4":
-			    decimal += 4;
-			    break;
-			case hex[i] == "5":
-			    decimal += 5;
-			    break;
-			case hex[i] == "6":
-			    decimal +=6;
-			    break;
-			case hex[i] == "7":
-			    decimal +=7;
-			    break;
-			case hex[i] == "8":
-			    decimal +=8;
-			    break;
-			case hex[i] == "9":
-			    decimal +=9;
-			    break;
-			case hex[i] == ("A" || "a"):
-			    decimal += 10;
-			    break;
-			case hex[i] == ("B" || "b"):
-			    decimal += 11;
-			    break;
-			case hex[i] == ("C" || "c"):
-			    decimal += 12;
-			    break;
-			case hex[i] == ("D" || "d"):
-			    decimal += 13;
-			    break;
-			case hex[i] == ("E" || "e"):
-			    decimal += 14;
-			    break;
-			case hex[i] == ("F" || "f"):
-			    decimal += 15;
-			    break;
-			default:
-			    decimal += 0;
-	    }
 	printf("decimal:%ld\n", decimal);
 	
 	return 0;
