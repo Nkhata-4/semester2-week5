392db0e513e8d9924f6de50fa096ab50c7ad0e1c
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-03-09 13:01:14 +0000

    worked on portfolio

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -35,37 +35,37 @@ int main(void){
 	
 	int len = strlen(hex);
 	while (*ptr != '\0') { 
-		if (ptr == "0") {
+		if (*ptr == '0') {
 			decimal = decimal;
-		} else if (ptr == "1") {
+		} else if (*ptr == '1') {
 			decimal += 1;
-		} else if (ptr == "2") {
+		} else if (*ptr == '2') {
 			decimal +=2;
-		} else if (ptr == "3") {
+		} else if (*ptr == '3') {
 			decimal +=3;
-		} else if (ptr == "4") {
+		} else if (*ptr == '4') {
 			decimal +=4;
-		} else if (ptr == "5") {
+		} else if (*ptr == '5') {
 			decimal +=5;
-		} else if (ptr == "6") {
+		} else if (*ptr == '6') {
 			decimal +=6;
-		} else if (ptr == "7") {
+		} else if (*ptr == '7') {
 			decimal += 7;
-		} else if (ptr == "8") {
+		} else if (*ptr == '8') {
 			decimal += 8;
-		} else if (ptr == "9") {
+		} else if (*ptr == '9') {
 			decimal += 9;
-		} else if (ptr == "A"||ptr == "a") {
+		} else if (*ptr == 'A'||*ptr == 'a') {
 			decimal += 10;
-		} else if (ptr == "B") {
+		} else if (*ptr == 'B'||*ptr == 'b') {
 			decimal +=11;
-		} else if (ptr == "C") {
+		} else if (*ptr == 'C'||*ptr == 'c') {
 			decimal += 12;
-		} else if (ptr == "D") {
+		} else if (*ptr == 'D'||*ptr == 'd') {
 			decimal += 13;
-		} else if (ptr == "E") {
+		} else if (*ptr == 'E'||*ptr == 'e') {
 			decimal += 14;
-		} else if (ptr == "F") {
+		} else if (*ptr == 'F'||*ptr == 'f') {
 			decimal += 15;
 		} else {
 			printf("Error: Invalid Hexadecimal\n");
