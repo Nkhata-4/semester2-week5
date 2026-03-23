bf774cae6d122260f01865405c0cd948ca8d7df2
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-03-23 12:49:24 +0000

    worked on portfolio task

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -26,68 +26,12 @@ int main(void){
 	// print the decimal result
 	
 	int len = strlen(hex);
-	/*
+	int i=0;
 	while (*ptr != '\0' && i<=len) {
-		if (len==1) {
-			int power=0;
-		} else {
-			int power=len-i;
-		}
-		power=len-i;
-		if (*ptr == '0'|| *ptr == '\0') {
-			decimal = decimal;
-		} else if (*ptr == '1') {
-			decimal += 1 * (16^power);
-		} else if (*ptr == '2') {
-			decimal +=2;
-		} else if (*ptr == '3') {
-			decimal +=3;
-		} else if (*ptr == '4') {
-			decimal +=4;
-		} else if (*ptr == '5') {
-			decimal +=5;
-		} else if (*ptr == '6') {
-			decimal +=6;
-		} else if (*ptr == '7') {
-			decimal += 7;
-		} else if (*ptr == '8') {
-			decimal += 8;
-		} else if (*ptr == '9') {
-			decimal += 9;
-		} else if (*ptr == 'A'||*ptr == 'a') {
-			decimal += 10 * (16^power);
-		} else if (*ptr == 'B'||*ptr == 'b') {
-			decimal +=11;
-		} else if (*ptr == 'C'||*ptr == 'c') {
-			decimal += 12;
-		} else if (*ptr == 'D'||*ptr == 'd') {
-			decimal += 13;
-		} else if (*ptr == 'E'||*ptr == 'e') {
-			decimal += 14;
-		} else if (*ptr == 'F'||*ptr == 'f') {
-			decimal += 15;
-		} else {
-			printf("Error: Invalid Hexadecimal\n");
-			return 0;
-		} 
-		ptr++;
-		i+=1;
-	}*/
-	/*
-	long j=3;
-	float multr = pow(16, 3);
-	printf("%f\n", multr);
-	return 0;
-	//long multr = pow(16, 2);
-    if (len == 1) {
-		multr = 0;
-	} else if (len == 2)
-	}*/
-
-	for (int i=0; i<len; i++) {
-		double j=1;
-		double power=len-j;
-        double multr=pow(16, power);
+		int j=i+1;
+		int exponent=len-j;
+		int base=16;
+        double multr=pow(base, exponent);
 		if (*ptr == '0'|| *ptr == '\0') {
 			decimal = decimal;
 		} else if (*ptr == '1') {
@@ -123,9 +67,11 @@ int main(void){
 		} else {
 			printf("Error: Invalid Hexadecimal\n");
 			return 0;
-		}
-		j++;
+		} 
+		ptr++;
+		i+=1;
 	}
+
 	printf("decimal:%ld\n", decimal);
 	return 0;
 }
